63e258e7e99efb4d32f146f010687bed7687090f
baijunyao <baijunyao@baijunyao.com>
2018-04-04 13:30:56 +0800

    :tada:初始化提交

---
+++ src/helpers.php
@@ -0,0 +1,36 @@
+<?php
+
+use PhpOffice\PhpWord\IOFactory;
+
+if ( !function_exists('word_to_text') ) {
+    /**
+     * 从 word 文件中获取纯本文文字
+     *
+     * @param $file
+     *
+     * @return string
+     */
+    function word_to_text($file)
+    {
+        return strip_tags(word_to_html($file));
+    }
+}
+
+if ( !function_exists('word_to_html') ) {
+    /**
+     * 从 word 文件中获取带样式的文本
+     *
+     * @param $file
+     *
+     * @return mixed
+     * @throws \PhpOffice\PhpWord\Exception\Exception
+     */
+    function word_to_html($file)
+    {
+        $phpWord = IOFactory::load($file);
+        $xmlWriter = IOFactory::createWriter($phpWord , 'HTML');
+        $html = $xmlWriter->getWriterPart('Body')->write();
+        $html = str_replace(['<body>', '</body>'], '', $html);
+        return $html;
+    }
+}
